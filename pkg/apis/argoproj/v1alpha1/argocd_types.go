02913af90de818b255e604b25e55cc51f3fd7474
John McKenzie <john@phobos.int.cloudkube.io>
2019-09-24 18:35:15 -0500

    Change group name

---
+++ pkg/apis/argoproj/v1alpha1/argocd_types.go
@@ -0,0 +1,50 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ArgoCDSpec defines the desired state of ArgoCD
+// +k8s:openapi-gen=true
+type ArgoCDSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// ArgoCDStatus defines the observed state of ArgoCD
+// +k8s:openapi-gen=true
+type ArgoCDStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ArgoCD is the Schema for the argocds API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+type ArgoCD struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ArgoCDSpec   `json:"spec,omitempty"`
+	Status ArgoCDStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ArgoCDList contains a list of ArgoCD
+type ArgoCDList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ArgoCD `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ArgoCD{}, &ArgoCDList{})
+}
---
+++ pkg/apis/argoproj/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the argoproj v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=argoproj.io
+package v1alpha1
