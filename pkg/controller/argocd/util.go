75422d82d53b36e11c37a8b9a33642f5ba301936
John McKenzie <john@phobos.int.cloudkube.io>
2019-11-04 10:53:56 -0600

    Add helpers for labels certificates

--- pkg/controller/argocd/util.go
+++ pkg/controller/argocd/util.go
@@ -27,8 +27,21 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
+const (
+	// ArgoCDAppName is the application name for labels.
+	ArgoCDAppName = "argocd"
+
+	// ArgoCDNameKey is the resource name key for labels.
+	ArgoCDNameKey = "app.kubernetes.io/name"
+
+	// ArgoCDPartOfKey is the resource part-of key for labels.
+	ArgoCDPartOfKey = "app.kubernetes.io/part-of"
+)
+
 var isOpenshiftCluster = false
 
+// isObjectFound will perform a basic check that the given object exists via the Kubernetes API.
+// If an error occurs as part of the check, the function will return false.
 func (r *ReconcileArgoCD) isObjectFound(nsname types.NamespacedName, obj runtime.Object) bool {
 	err := r.client.Get(context.TODO(), nsname, obj)
 	if err != nil {
@@ -69,6 +82,19 @@ func VerifyOpenShift() error {
 	return nil
 }
 
+// reconcileCertificateAuthority will reconcile all Certificate Authority resources.
+func (r *ReconcileArgoCD) reconcileCertificateAuthority(cr *argoproj.ArgoCD) error {
+	if err := r.reconcileCASecret(cr); err != nil {
+		return err
+	}
+
+	if err := r.reconcileCAConfigMap(cr); err != nil {
+		return err
+	}
+	return nil
+}
+
+// reconcileOpenShiftResources will reconcile OpenShift specific ArgoCD resources.
 func (r *ReconcileArgoCD) reconcileOpenShiftResources(cr *argoproj.ArgoCD) error {
 	if err := r.reconcileRoutes(cr); err != nil {
 		return err
@@ -92,12 +118,17 @@ func (r *ReconcileArgoCD) reconcileOpenShiftResources(cr *argoproj.ArgoCD) error
 	return nil
 }
 
+// reconcileResources will reconcile common ArgoCD resources.
 func (r *ReconcileArgoCD) reconcileResources(cr *argoproj.ArgoCD) error {
-	if err := r.reconcileConfigMaps(cr); err != nil {
+	if err := r.reconcileCertificateAuthority(cr); err != nil {
 		return err
 	}
 
 	if err := r.reconcileSecrets(cr); err != nil {
+		return err
+	}
+
+	if err := r.reconcileConfigMaps(cr); err != nil {
 		return err
 	}
 
@@ -109,4 +140,36 @@ func (r *ReconcileArgoCD) reconcileResources(cr *argoproj.ArgoCD) error {
 		return err
 	}
 	return nil
+}
+
+// defaultLabels returns the default set of labels for the cluster.
+func defaultLabels(cr *argoproj.ArgoCD) map[string]string {
+	return map[string]string{
+		ArgoCDNameKey:   cr.Name,
+		ArgoCDPartOfKey: ArgoCDAppName,
+	}
+}
+
+// labelsForCluster returns the labels for all cluster resources.
+func labelsForCluster(cr *argoproj.ArgoCD) map[string]string {
+	labels := defaultLabels(cr)
+	for key, val := range cr.ObjectMeta.Labels {
+		labels[key] = val
+	}
+	return labels
+}
+
+// setDefaults sets the default vaules for the spec and returns true if the spec was changed.
+func setDefaults(cr *argoproj.ArgoCD) bool {
+	changed := false
+	return changed
+}
+
+// withClusterLabels will add the given labels to the labels for the cluster and return the result.
+func withClusterLabels(cr *argoproj.ArgoCD, addLabels map[string]string) map[string]string {
+	labels := labelsForCluster(cr)
+	for key, val := range addLabels {
+		labels[key] = val
+	}
+	return labels
 }
