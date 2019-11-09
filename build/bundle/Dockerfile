9734b329ddf55792587892f8759ca1d71c0ddfee
John McKenzie <jmckind@gmail.com>
2019-11-09 13:17:24 -0600

    Add artifacts for generating operator bundle

---
+++ build/bundle/Dockerfile
@@ -0,0 +1,12 @@
+FROM quay.io/operator-framework/upstream-registry-builder as builder
+
+COPY manifests manifests
+RUN ./bin/initializer -o ./bundles.db
+
+FROM scratch
+COPY --from=builder /build/bundles.db /bundles.db
+COPY --from=builder /build/bin/registry-server /registry-server
+COPY --from=builder /bin/grpc_health_probe /bin/grpc_health_probe
+EXPOSE 50051
+ENTRYPOINT ["/registry-server"]
+CMD ["--database", "bundles.db"]
--- hack/bundle.sh
+++ hack/bundle.sh
@@ -5,28 +5,16 @@ set -e
 HACK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${HACK_DIR}/env.sh
 
-# Verify bundle
-operator-courier --verbose verify ${ARGOCD_OPERATOR_BUNDLE_DIR}
+# Create bundle build directory
+mkdir -p ${ARGOCD_OPERATOR_BUNDLE_BUILD_DIR}
 
-# Obtain Quay crednetials
-if [ -z ${QUAY_USERNAME} ]; then
-    echo -n "Username: "
-    read QUAY_USERNAME
-fi
+# Copy bundle artifacts
+cp -r ${ARGOCD_OPERATOR_BUNDLE_DIR}/* ${ARGOCD_OPERATOR_BUNDLE_BUILD_DIR}/
 
-if [ -z ${QUAY_PASSWORD} ]; then
-    echo -n "Password: "
-    read -s QUAY_PASSWORD
-fi
+# Copy manifests 
+mkdir -p ${ARGOCD_OPERATOR_BUNDLE_BUILD_DIR}/manifests
+cp -r ${ARGOCD_OPERATOR_BUNDLE_MANIFEST_DIR} ${ARGOCD_OPERATOR_BUNDLE_BUILD_DIR}/manifests/
 
-# Obtain Quay auth token
-QUAY_TOKEN=$(curl -sH "Content-Type: application/json" -XPOST ${QUAY_LOGIN_URL} -d '
-{
-    "user": {
-        "username": "'"${QUAY_USERNAME}"'",
-        "password": "'"${QUAY_PASSWORD}"'"
-    }
-}' | awk -F '"' '{print $4}')
-
-# Publish bundle
-operator-courier --verbose push ${ARGOCD_OPERATOR_BUNDLE_DIR} ${ARGOCD_OPERATOR_BUNDLE_NAMESPACE} ${ARGOCD_OPERATOR_BUNDLE_REPO} ${ARGOCD_OPERATOR_BUNDLE_RELEASE} "${QUAY_TOKEN}"
+# Build the bundle registry container image
+docker build -t ${ARGOCD_OPERATOR_BUNDLE_IMAGE} ${ARGOCD_OPERATOR_BUNDLE_BUILD_DIR}
+docker push ${ARGOCD_OPERATOR_BUNDLE_IMAGE}
