503860eefc97225d0332c30af6e05b0305a92ad5
John McKenzie <jmckind@gmail.com>
2020-05-26 10:12:52 -0500

    Set config dirs for local development

--- hack/local.sh
+++ hack/local.sh
@@ -17,6 +17,9 @@
 HACK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${HACK_DIR}/env.sh
 
+export GRAFANA_CONFIG_PATH="$(pwd)/grafana"
+export REDIS_CONFIG_PATH="$(pwd)/build/redis"
+
 operator-sdk run \
     --local \
     --operator-namespace ${ARGOCD_OPERATOR_NAMESPACE} \
