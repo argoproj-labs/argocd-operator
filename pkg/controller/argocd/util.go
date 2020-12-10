68526f99804c4a3fbf1bf197784b32d1b38dabd0
Shoubhik Bose <shbose@redhat.com>
2020-12-09 21:56:37 -0500

    Update pkg/controller/argocd/util.go
    Co-authored-by: William Tam <wtam@redhat.com>

--- pkg/controller/argocd/util.go
+++ pkg/controller/argocd/util.go
@@ -640,13 +640,13 @@ func labelsForCluster(cr *argoprojv1a1.ArgoCD) map[string]string {
 	return labels
 }
 
-// annotationsForCluster returns the labels for all cluster resources.
+// annotationsForCluster returns the annotations for all cluster resources.
 func annotationsForCluster(cr *argoprojv1a1.ArgoCD) map[string]string {
-	labels := argoutil.DefaultAnnotations(cr)
+	annotations := argoutil.DefaultAnnotations(cr)
 	for key, val := range cr.ObjectMeta.Annotations {
-		labels[key] = val
+		annotations[key] = val
 	}
-	return labels
+	return annotations
 }
 
 // setDefaults sets the default vaules for the spec and returns true if the spec was changed.
