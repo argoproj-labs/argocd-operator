67c03c605af93f8cef5b708e76d4c9052abdd1c1
Kevin McDermott <bigkevmcd@gmail.com>
2020-12-17 15:48:52 +0000

    Fix argocd server insecure (#220)
    This addresses a bug in the argocd-server deployment when changing the
    routing to be insecure.
    
    Previously the command-line was not being updated when it was being
    rereconciled after changing.

--- pkg/controller/argocd/deployment.go
+++ pkg/controller/argocd/deployment.go
@@ -1062,6 +1062,11 @@ func (r *ReconcileArgoCD) reconcileServerDeployment(cr *argoprojv1a1.ArgoCD) err
 			existing.Spec.Template.Spec.Containers[0].Env = deploy.Spec.Template.Spec.Containers[0].Env
 			changed = true
 		}
+		if !reflect.DeepEqual(existing.Spec.Template.Spec.Containers[0].Command,
+			deploy.Spec.Template.Spec.Containers[0].Command) {
+			existing.Spec.Template.Spec.Containers[0].Command = deploy.Spec.Template.Spec.Containers[0].Command
+			changed = true
+		}
 		if changed {
 			return r.client.Update(context.TODO(), existing)
 		}
