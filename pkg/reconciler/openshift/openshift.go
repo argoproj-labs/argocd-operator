d96b6e324c026d713696fa6913bc5326679a7e11
Shubham Agarwal <shubhamagarawal19@gmail.com>
2021-03-16 21:30:18 +0530

    support compliance.openshift.io apiGroup for cluster config (#267)
    * support compliance.openshift.io apiGroup ootb for cluster config
    
    * updated the permissions

--- pkg/reconciler/openshift/openshift.go
+++ pkg/reconciler/openshift/openshift.go
@@ -147,6 +147,32 @@ func policyRulesForClusterConfig() []rbacv1.PolicyRule {
 			Verbs: []string{
 				"*",
 			},
+		}, {
+			APIGroups: []string{
+				"compliance.openshift.io",
+			},
+			Resources: []string{
+				"scansettings",
+				"compliancesuites",
+				"compliancescans",
+				"compliancecheckresults",
+				"complianceremediations",
+			},
+			Verbs: []string{
+				"get",
+				"watch",
+				"list",
+			},
+		},  {
+			APIGroups: []string{
+				"compliance.openshift.io",
+			},
+			Resources: []string{
+				"scansettingbindings",
+			},
+			Verbs: []string{
+				"*",
+			},
 		},
 	}
 }
