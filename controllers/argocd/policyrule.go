948d12ce98bdaf335705d43bcdc6c86349c070e4
John Pitman <jpitman@redhat.com>
2022-07-24 10:37:31 -0400

    Fix: add policy rules for redis on OpenShift
    Signed-off-by: John Pitman <jpitman@redhat.com>

--- controllers/argocd/policyrule.go
+++ controllers/argocd/policyrule.go
@@ -26,6 +26,16 @@ func policyRuleForApplicationController() []v1.PolicyRule {
 	}
 }
 
+func policyRuleForRedis(client client.Client) []v1.PolicyRule {
+	rules := []v1.PolicyRule{}
+
+	// Need additional policy rules if we are running on openshift, else the stateful set won't have the right
+	// permissions to start
+	rules = appendOpenShiftNonRootSCC(rules, client)
+
+	return rules
+}
+
 func policyRuleForRedisHa(client client.Client) []v1.PolicyRule {
 
 	rules := []v1.PolicyRule{
