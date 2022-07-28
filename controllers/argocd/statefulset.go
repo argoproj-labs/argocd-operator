793bd6ee667d0617781784e93840e133ce4097c5
saumeya <saumeyakatyal@gmail.com>
2022-07-28 14:25:29 +0530

    fix: remove liveness probe in 0.3 operator

--- controllers/argocd/statefulset.go
+++ controllers/argocd/statefulset.go
@@ -367,21 +367,7 @@ func (r *ReconcileArgoCD) reconcileApplicationControllerStatefulSet(cr *argoproj
 		Image:           getArgoContainerImage(cr),
 		ImagePullPolicy: corev1.PullAlways,
 		Name:            "argocd-application-controller",
-<<<<<<< HEAD
-		LivenessProbe: &corev1.Probe{
-			Handler: corev1.Handler{
-				HTTPGet: &corev1.HTTPGetAction{
-					Path: "/healthz",
-					Port: intstr.FromInt(8082),
-				},
-			},
-			InitialDelaySeconds: 5,
-			PeriodSeconds:       10,
-		},
-		Env: controllerEnv,
-=======
 		Env:             controllerEnv,
->>>>>>> e423064 (fix: remove liveness probe from application controller statefulset (#742))
 		Ports: []corev1.ContainerPort{
 			{
 				ContainerPort: 8082,
