515ad71f80fa9314783977c06cf7b896bd4296fb
rishabh625 <rishabhmishra625@gmail.com>
2022-08-17 23:15:19 +0530

    merged master branch and resolved conflicts
    Signed-off-by: rishabh625 <rishabhmishra625@gmail.com>

--- controllers/argocd/applicationset.go
+++ controllers/argocd/applicationset.go
@@ -74,7 +74,6 @@ func (r *ReconcileArgoCD) reconcileApplicationSetController(cr *argoprojv1a1.Arg
 		return err
 	}
 
-<<<<<<< HEAD
 	log.Info("reconciling applicationset ingress")
 	if err := r.reconcileApplicationSetControllerIngress(cr); err != nil {
 		return err
@@ -85,8 +84,6 @@ func (r *ReconcileArgoCD) reconcileApplicationSetController(cr *argoprojv1a1.Arg
 		return err
 	}
 
-=======
->>>>>>> master
 	log.Info("reconciling applicationset service")
 	if err := r.reconcileApplicationSetService(cr); err != nil {
 		return err
