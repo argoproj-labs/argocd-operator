dbb071488204d6d1d0b4ff6b0456cfa27a82062c
iam-veeramalla <abhishek.veeramalla@gmail.com>
2022-12-14 01:33:23 +0530

    chore: prepare release manifests
    Signed-off-by: iam-veeramalla <abhishek.veeramalla@gmail.com>

--- common/defaults.go
+++ common/defaults.go
@@ -119,7 +119,7 @@ const (
 	ArgoCDDefaultExportJobImage = "quay.io/argoprojlabs/argocd-operator-util"
 
 	// ArgoCDDefaultExportJobVersion is the export job container image tag to use when not specified.
-	ArgoCDDefaultExportJobVersion = "sha256:133b064fc01154efd6b1a1698571b0e461d4b515245bbff3350bbb186a7f6935" // 0.4.0
+	ArgoCDDefaultExportJobVersion = "sha256:6f80965a2bef1c80875be0995b18d9be5a6ad4af841cbc170ed3c60101a7deb2" // 0.5.0
 
 	// ArgoCDDefaultExportLocalCapicity is the default capacity to use for local export.
 	ArgoCDDefaultExportLocalCapicity = "2Gi"
