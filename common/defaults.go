770f8f0aa55c751bfb23f8d3a88a0e04b188db5f
Jaideep Rao <jaideep.r97@gmail.com>
2023-01-17 15:59:24 -0500

    Revert "feat: Expose instance level metrics (#830)" (#843)
    This reverts commit 1b1a07a2d2dd57b1eb5b833886ea64cb5b7fbfcd.

--- common/defaults.go
+++ common/defaults.go
@@ -237,9 +237,6 @@ const (
 	// ArgoCDDefaultRepoMetricsPort is the default listen port for the Argo CD repo server metrics.
 	ArgoCDDefaultRepoMetricsPort = 8084
 
-	// ArgoCDDefaultInstanceStatusMetricsPort is the default listen port for the Argo CD workload status metrics.
-	ArgoCDDefaultInstanceStatusMetricsPort = 8085
-
 	// ArgoCDDefaultRepoServerPort is the default listen port for the Argo CD repo server.
 	ArgoCDDefaultRepoServerPort = 8081
 
