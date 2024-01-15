9fdc111e6af3447addef4dc1650d19f0e6db7461
Jaideep Rao <jaideep.r97@gmail.com>
2024-01-15 18:18:40 -0500

    add append and isnil methods
    Signed-off-by: Jaideep Rao <jaideep.r97@gmail.com>

--- pkg/util/error.go
+++ pkg/util/error.go
@@ -10,6 +10,19 @@ func (e *MultiError) Unwrap() []error {
 	return e.Errs
 }
 
-func (e *MultiError) Error() string {
+func (e MultiError) Error() string {
 	return errors.Join(e.Errs...).Error()
+}
+
+func (e *MultiError) Append(errs ...error) {
+	for _, err := range errs {
+		if err == nil {
+			continue
+		}
+		e.Errs = append(e.Errs, err)
+	}
+}
+
+func (e *MultiError) IsNil() bool {
+	return len(e.Errs) == 0
 }
