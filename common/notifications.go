540a5f07e4f6dd4f0d5f6e9a5e99195dd7fc5ea5
Abhishek Veeramalla <abhishek.veeramalla@gmail.com>
2024-02-01 17:19:52 +0530

    pull in notifications changes
    Signed-off-by: Jaideep Rao <jaideep.r97@gmail.com>

--- common/notifications.go
+++ common/notifications.go
@@ -6,4 +6,6 @@ const (
 	NotificationsControllerComponent = "argocd-notifications-controller"
 	NotificationsSecretName          = "argocd-notifications-secret"
 	NotificationsConfigMapName       = "argocd-notifications-cm"
+	// NotificationsControllerMetricsPort is the port that is used to expose notifications controller metrics.
+	NotificationsControllerMetricsPort = 9001
 )
