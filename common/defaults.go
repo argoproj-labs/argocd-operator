898c361d5fd8e567ce8f50873a1ce3d73d55c9d2
saumeya <saumeyakatyal@gmail.com>
2024-08-27 17:02:20 +0530

    updated to amd64 images
    Signed-off-by: saumeya <saumeyakatyal@gmail.com>

--- common/defaults.go
+++ common/defaults.go
@@ -128,7 +128,7 @@ const (
 	ArgoCDDefaultExportJobImage = "quay.io/argoprojlabs/argocd-operator-util"
 
 	// ArgoCDDefaultExportJobVersion is the export job container image tag to use when not specified.
-	ArgoCDDefaultExportJobVersion = "sha256:800b0353d566b5c2f269f0586f453ec86ff7cc93877e581f47b039d7f2333cb9" // 0.12.0
+	ArgoCDDefaultExportJobVersion = "sha256:43f74879ce38af1e0ce37dc159332efd282b63da3eda43e71de9cecfa45df153" // 0.12.0
 
 	// ArgoCDDefaultExportLocalCapicity is the default capacity to use for local export.
 	ArgoCDDefaultExportLocalCapicity = "2Gi"
