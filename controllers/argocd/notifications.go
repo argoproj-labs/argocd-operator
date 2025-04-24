d0b8ed3882c1bdb021e4b399537dede333cb1cfe
Jonathan West <jgwest@users.noreply.github.com>
2025-04-24 08:30:11 -0400

    Upgrade to latest prometheus-operator (#1707)
    Signed-off-by: Jonathan West <jonwest@redhat.com>

--- controllers/argocd/notifications.go
+++ controllers/argocd/notifications.go
@@ -6,7 +6,7 @@ import (
 	"reflect"
 	"time"
 
-	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
