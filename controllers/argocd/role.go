f4ace81b132661052605d422c48d4682c8e8ff12
Alka Kumari <alkumari@redhat.com>
2025-05-30 12:14:39 +0530

    Removed a typo that resulted in test case failures
    Signed-off-by: Alka Kumari <alkumari@redhat.com>

--- controllers/argocd/role.go
+++ controllers/argocd/role.go
@@ -518,7 +518,7 @@ func matchAggregatedClusterRoleFields(expectedClusterRole *v1.ClusterRole, exist
 
 	// if ClusterRole is for View permissions then compare Labels
 	if name == common.ArgoCDApplicationControllerComponentView {
-		if !AddExistingLabels(&existingClusterRole.Labels, expectedClusterRole.Labels) {
+		if AddExistingLabels(&existingClusterRole.Labels, expectedClusterRole.Labels) {
 			if changed {
 				explanation += ", "
 			}
