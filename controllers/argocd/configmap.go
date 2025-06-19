78442d980528becce283b885585c76f178a61548
Rizwana Naaz <rizwananaaz177@gmail.com>
2025-06-19 10:19:24 +0530

    feat: Create argocd-cmd-params-cm configMap (#1756)
    Signed-off-by: Rizwana777 <rizwananaaz177@gmail.com>
    (cherry picked from commit 6711c5974316ae0045d0cdeb6c3b4f547adf0bf3)
    Signed-off-by: nmirasch <neus.miras@gmail.com>

--- controllers/argocd/configmap.go
+++ controllers/argocd/configmap.go
@@ -348,6 +348,10 @@ func (r *ReconcileArgoCD) reconcileConfigMaps(cr *argoproj.ArgoCD, useTLSForRedi
 		return err
 	}
 
+	if err := r.reconcileArgoCmdParamsConfigMap(cr); err != nil {
+		return err
+	}
+
 	return r.reconcileGPGKeysConfigMap(cr)
 }
 
@@ -802,4 +806,51 @@ func (r *ReconcileArgoCD) reconcileGPGKeysConfigMap(cr *argoproj.ArgoCD) error {
 	}
 	argoutil.LogResourceCreation(log, cm)
 	return r.Client.Create(context.TODO(), cm)
+}
+
+// reconcileArgoCmdParamsConfigMap will ensure that the ConfigMap containing command line parameters for ArgoCD is present.
+func (r *ReconcileArgoCD) reconcileArgoCmdParamsConfigMap(cr *argoproj.ArgoCD) error {
+	cm := newConfigMapWithName(common.ArgoCDCmdParamsConfigMapName, cr)
+	cm.Data = make(map[string]string)
+
+	// Only set data if spec.cmdParams is defined
+	if len(cr.Spec.CmdParams) > 0 {
+		for k, v := range cr.Spec.CmdParams {
+			cm.Data[k] = v
+		}
+	}
+
+	if err := controllerutil.SetControllerReference(cr, cm, r.Scheme); err != nil {
+		return err
+	}
+	existingCM := &corev1.ConfigMap{}
+	if argoutil.IsObjectFound(r.Client, cr.Namespace, cm.Name, existingCM) {
+		changed := false
+
+		// Compare only if data is being managed
+		if len(cm.Data) > 0 && !reflect.DeepEqual(cm.Data, existingCM.Data) {
+			existingCM.Data = cm.Data
+			changed = true
+		}
+		// Check OwnerReferences
+		var refChanged bool
+		var err error
+		if refChanged, err = modifyOwnerReferenceIfNeeded(cr, existingCM, r.Scheme); err != nil {
+			return err
+		}
+		if refChanged {
+			changed = true
+		}
+		if changed {
+			explanation := "updating data"
+			if refChanged {
+				explanation += ", owner reference"
+			}
+			argoutil.LogResourceUpdate(log, existingCM, explanation)
+			return r.Update(context.TODO(), existingCM)
+		}
+		return nil // Do nothing as there is no change in the configmap.
+	}
+	argoutil.LogResourceCreation(log, cm)
+	return r.Create(context.TODO(), cm)
 }
