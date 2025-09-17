5ec286a6df0ab3334748d7dcc0d81b964af22900
Alka Kumari <alkumari@redhat.com>
2025-09-17 17:00:38 +0530

    change call to UpdateMapValues in repo_server.go file
    Signed-off-by: Alka Kumari <alkumari@redhat.com>

--- controllers/argocd/repo_server.go
+++ controllers/argocd/repo_server.go
@@ -506,8 +506,8 @@ func (r *ReconcileArgoCD) reconcileRepoDeployment(cr *v1beta1.ArgoCD, useTLSForR
 		}
 
 		// Add Kubernetes-specific labels/annotations from the live object in the source to preserve metadata.
-		addKubernetesData(deploy.Spec.Template.Labels, existing.Spec.Template.Labels)
-		addKubernetesData(deploy.Spec.Template.Annotations, existing.Spec.Template.Annotations)
+		UpdateMapValues(&existing.Spec.Template.Labels, deploy.Spec.Template.Labels)
+		UpdateMapValues(&existing.Spec.Template.Annotations, deploy.Spec.Template.Annotations)
 
 		if !reflect.DeepEqual(deploy.Spec.Template.Annotations, existing.Spec.Template.Annotations) {
 			existing.Spec.Template.Annotations = deploy.Spec.Template.Annotations
