d07b97bb35bb3cd0cbb752c9fd1eadd5a530c812
akhilnittala <nakhil@redhat.com>
2025-11-12 09:21:10 +0530

    fix: redis ha proxy rolling update pod in pending state (#1936)
    Signed-off-by: akhil nittala <nakhil@redhat.com>

--- controllers/argocd/deployment.go
+++ controllers/argocd/deployment.go
@@ -646,7 +646,12 @@ func (r *ReconcileArgoCD) reconcileRedisDeployment(cr *argoproj.ArgoCD, useTLS b
 // reconcileRedisHAProxyDeployment will ensure the Deployment resource is present for the Redis HA Proxy component.
 func (r *ReconcileArgoCD) reconcileRedisHAProxyDeployment(cr *argoproj.ArgoCD) error {
 	deploy := newDeploymentWithSuffix("redis-ha-haproxy", "redis", cr)
-
+	deploy.Spec.Strategy = appsv1.DeploymentStrategy{
+		Type: appsv1.RollingUpdateDeploymentStrategyType,
+		RollingUpdate: &appsv1.RollingUpdateDeployment{
+			MaxSurge: &intstr.IntOrString{IntVal: 0},
+		},
+	}
 	var redisEnv = append(proxyEnvVars(), corev1.EnvVar{
 		Name: "AUTH",
 		ValueFrom: &corev1.EnvVarSource{
@@ -929,6 +934,14 @@ func (r *ReconcileArgoCD) reconcileRedisHAProxyDeployment(cr *argoproj.ArgoCD) e
 				explanation += ", "
 			}
 			explanation += "pod security context"
+			changed = true
+		}
+		if !reflect.DeepEqual(deploy.Spec.Strategy, existing.Spec.Strategy) {
+			existing.Spec.Strategy = deploy.Spec.Strategy
+			if changed {
+				explanation += ", "
+			}
+			explanation += "deployment strategy"
 			changed = true
 		}
 		if changed {
