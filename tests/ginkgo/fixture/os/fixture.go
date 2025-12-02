dd002effa460bd70f07fcbbf706dd9ddc3a5a00e
Jonathan West <jonwest@redhat.com>
2025-12-02 04:07:15 -0500

    chore: don't output 'df -k' command string
    Signed-off-by: Jonathan West <jonwest@redhat.com>

--- tests/ginkgo/fixture/os/fixture.go
+++ tests/ginkgo/fixture/os/fixture.go
@@ -8,12 +8,15 @@ import (
 )
 
 func ExecCommand(cmdArgs ...string) (string, error) {
-	return ExecCommandWithOutputParam(true, cmdArgs...)
+	return ExecCommandWithOutputParam(true, true, cmdArgs...)
 }
 
 // You probably want to use ExecCommand, unless you need to supress the output of sensitive data (for example, openssl CLI output)
-func ExecCommandWithOutputParam(printOutput bool, cmdArgs ...string) (string, error) {
-	GinkgoWriter.Println("executing command:", cmdArgs)
+func ExecCommandWithOutputParam(printOutput bool, printCommand bool, cmdArgs ...string) (string, error) {
+
+	if printCommand {
+		GinkgoWriter.Println("executing command:", cmdArgs)
+	}
 
 	// #nosec G204
 	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
