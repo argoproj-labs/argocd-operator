0ad7f823d58083825a97a9cc0f96b552b2eb5be4
akhil nittala <nakhil@redhat.com>
2025-12-21 08:38:24 +0530

    [GITOPS-8560]: Validation webhook to reject invalid SSO configs in ArgoCD CR
    Signed-off-by: akhil nittala <nakhil@redhat.com>

--- api/v1beta1/argocd_webhook.go
+++ api/v1beta1/argocd_webhook.go
@@ -17,11 +17,54 @@ limitations under the License.
 package v1beta1
 
 import (
+	"context"
+	"errors"
+
+	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
-func (r *ArgoCD) SetupWebhookWithManager(mgr ctrl.Manager) error {
+type ArgoCDValidator struct {
+	// TODO(user): Add more fields as needed for validation
+}
+
+var _ admission.CustomValidator = &ArgoCDValidator{}
+
+func SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).
-		For(r).
+		For(&ArgoCD{}).WithValidator(&ArgoCDValidator{}).
 		Complete()
+}
+
+//+kubebuilder:webhook:path=/validate-argoproj-io-v1beta1-argocd,mutating=false,failurePolicy=fail,sideEffects=None,groups=argoproj.io,resources=argocds,verbs=create;update,versions=v1beta1,name=vargocd.kb.io,admissionReviewVersions=v1
+
+// ValidateCreate implements admission.CustomValidator so a webhook will be registered for the type
+func (v *ArgoCDValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	argocd, ok := obj.(*ArgoCD)
+	if !ok {
+		return nil, errors.New("expected ArgoCD object")
+	}
+	if argocd.Spec.SSO != nil && argocd.Spec.OIDCConfig != "" {
+		return nil, errors.New("SSO and OIDCConfig cannot be used together")
+	}
+	return nil, nil
+}
+
+// ValidateUpdate implements admission.CustomValidator so a webhook will be registered for the type
+func (v *ArgoCDValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+	argocd, ok := newObj.(*ArgoCD)
+	if !ok {
+		return nil, errors.New("expected ArgoCD object")
+	}
+	if argocd.Spec.SSO != nil && argocd.Spec.OIDCConfig != "" {
+		return nil, errors.New("SSO and OIDCConfig cannot be used together")
+	}
+	return nil, nil
+}
+
+// ValidateDelete implements admission.CustomValidator so a webhook will be registered for the type
+func (v *ArgoCDValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	// Add any deletion validation logic here if needed
+	return nil, nil
 }
