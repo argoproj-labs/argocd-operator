da37f5369b8df33f7070eabb81020b7cc6904ede
Alka Kumari <alkumari@redhat.com>
2025-12-29 18:07:01 +0530

    commit for retriggering the pipeline
    Signed-off-by: Alka Kumari <alkumari@redhat.com>

--- tests/ginkgo/parallel/1-122_validate_argocd_reconciliation_timeout.go
+++ tests/ginkgo/parallel/1-122_validate_argocd_reconciliation_timeout.go
@@ -93,7 +93,7 @@ var _ = Describe("GitOps Operator Parallel E2E Tests", func() {
 
 		})
 
-		It("should set ARGOCD_RECONCILIATION_TIMEOUT with timeout.reconciliation value in configmap", func() {
+		It("should set environment variable ARGOCD_RECONCILIATION_TIMEOUT with timeout.reconciliation value in configmap", func() {
 			ns, cleanupFunc := fixture.CreateRandomE2ETestNamespaceWithCleanupFunc()
 			defer cleanupFunc()
 
