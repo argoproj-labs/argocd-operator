bcc877f057af8de5e2aa69b846083e42eb000ee4
Yonatan Sasson <yonatanxd7@gmail.com>
2026-01-27 14:11:43 +0200

    Fix typo
    Signed-off-by: Yonatan Sasson <yonatanxd7@gmail.com>

--- api/v1beta1/argocd_types.go
+++ api/v1beta1/argocd_types.go
@@ -1010,7 +1010,7 @@ type ArgoCDSpec struct {
 	// ClusterDomain is the cluster domain suffix used for constructing service FQDNs. Defaults to "cluster.local".
 	// The full FQDN will be: <service>.<namespace>.svc.<clusterDomain>
 	// This is useful for clusters that use a different DNS suffix (e.g., "CLUSTER_ID.cluster.local", "edge.local").
-	//+operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Cluster Domain'",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:text","urn:alm:descriptor:com.tectonic.ui:advanced"}
+	//+operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Cluster Domain",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:text","urn:alm:descriptor:com.tectonic.ui:advanced"}
 	ClusterDomain string `json:"clusterDomain,omitempty"`
 
 	// Banner defines an additional banner to be displayed in Argo CD UI
