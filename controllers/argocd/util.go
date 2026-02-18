004fd1f1d7d3e8687fa620b411717889c798ef1f
Anand Kumar Singh <anandrkskd@gmail.com>
2026-02-18 18:12:30 +0530

    feat: add network policy for argocd component (#2049)
    * add network policy for argocd component
    
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>
    
    * add networkPolicy field to crd, add tests, add update logic for repo-server, server, application-controller
    
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>
    
    * update tests
    
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>
    
    * rebase
    
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>
    
    * dont affect redis NP
    
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>
    
    * update NP functions
    
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>
    
    * update and rebase
    
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>
    
    * update unit test
    
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>
    
    * fix lint
    
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>
    
    * fix error and appset podselector
    
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>
    
    ---------
    
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>

--- controllers/argocd/util.go
+++ controllers/argocd/util.go
@@ -1041,6 +1041,23 @@ func (r *ReconcileArgoCD) setResourceWatches(bldr *builder.Builder, clusterResou
 
 	// Watch for changes to ConfigMap sub-resources owned by ArgoCD instances.
 	bldr.Owns(&corev1.ConfigMap{})
+	bldr.Owns(&corev1.Secret{})
+	bldr.Owns(&corev1.Service{})
+	bldr.Owns(&appsv1.Deployment{})
+	bldr.Owns(&networkingv1.Ingress{})
+	bldr.Owns(&appsv1.StatefulSet{})
+
+	// Watch for changes to NetworkPolicy sub-resources owned by ArgoCD instances.
+	// This ensures that if a NetworkPolicy is deleted, the controller reconciles and recreates it.
+	bldr.Owns(&networkingv1.NetworkPolicy{ObjectMeta: metav1.ObjectMeta{
+		Labels: map[string]string{
+			common.ArgoCDKeyPartOf: common.ArgoCDAppName,
+		},
+	}})
+
+	bldr.Owns(&v1.Role{})
+	bldr.Owns(&v1.RoleBinding{})
+	bldr.Owns(&v1alpha1.NotificationsConfiguration{})
 
 	// Watch for changes to Secret sub-resources owned by ArgoCD instances.
 	bldr.Owns(&corev1.Secret{})
