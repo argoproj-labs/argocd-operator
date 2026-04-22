110b330e1e56ddc3f07ebd091bd31c504b00a3f8
Alka Kumari <48486620+alkakumari016@users.noreply.github.com>
2026-04-22 15:12:37 +0530

    fix: restore serving-cert annotation when tls termination is reencrypt that was stripped in older versions (#2162)
    * restore serving-cert annotation when tls termination is reencrypt that was stripped in older versions
    
    Signed-off-by: Alka Kumari <alkumari@redhat.com>
    
    * added annotation constant and refacttored isCreatedByServiceCA
    
    Signed-off-by: Alka Kumari <alkumari@redhat.com>
    
    * cincorporated changes to create certs while test execution
    
    Signed-off-by: Alka Kumari <alkumari@redhat.com>
    
    * moved cert generation file inside fixture folder
    
    Signed-off-by: Alka Kumari <alkumari@redhat.com>
    
    ---------
    
    Signed-off-by: Alka Kumari <alkumari@redhat.com>

--- common/annotations.go
+++ common/annotations.go
@@ -12,4 +12,8 @@ const (
 	// AnnotationOpenShiftServiceCA is the annotation on services used to
 	// request a TLS certificate from OpenShift's Service CA for AutoTLS
 	AnnotationOpenShiftServiceCA = "service.beta.openshift.io/serving-cert-secret-name"
+
+	// AnnotationOpenShiftOriginatingServiceName is the annotation on secrets used to
+	// identify the service that created the secret.
+	AnnotationOpenShiftOriginatingServiceName = "service.beta.openshift.io/originating-service-name"
 )
