888474273eba497a6a7e21fcdb7ce5fb92b45f22
akhilnittala <nakhil@redhat.com>
2026-06-30 09:42:49 +0530

    fix: Missing permission for web-based terminal in Argocd UI (#2266)
    * fix(redis): Regression for masterauth field causing NOAUTH Authentication required error
    
    Signed-off-by: akhil nittala <nakhil@redhat.com>
    
    * fix(redis): Regression for masterauth field causing NOAUTH Authentication required error
    
    Signed-off-by: akhil nittala <nakhil@redhat.com>
    
    * fix(redis): Regression for masterauth field causing NOAUTH Authentication required error
    
    Signed-off-by: akhil nittala <nakhil@redhat.com>
    
    * Missing permission for web-based terminal in Argocd  UI
    
    Signed-off-by: akhil nittala <nakhil@redhat.com>
    
    ---------
    
    Signed-off-by: akhil nittala <nakhil@redhat.com>

--- controllers/argocd/service_account.go
+++ controllers/argocd/service_account.go
@@ -58,7 +58,7 @@ func getServiceAccountName(crName, name string) string {
 
 // reconcileServiceAccounts will ensure that all ArgoCD Service Accounts are configured.
 func (r *ReconcileArgoCD) reconcileServiceAccounts(cr *argoproj.ArgoCD) error {
-	params := getPolicyRuleList(r.Client)
+	params := getPolicyRuleList(r.Client, cr)
 
 	for _, param := range params {
 		if err := r.reconcileServiceAccountPermissions(param.name, param.policyRule, cr); err != nil {
